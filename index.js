e86b3ba1018baab6b6af6fab15f6e442176f325f
Vanessa Maldonado <vbarajas4@gmail.com>
2021-05-15 23:01:39 -0700

    final updates after video

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const inquirer = require ("inquirer");
-       
+const inquirer = require ("inquirer");       
 const fs = require("fs");
+
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
